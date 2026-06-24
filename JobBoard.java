c31f34f4d3a839bb5389f6363dc25587832ea299
Elior <elior@gmail.com>
2026-06-24 14:53:58 +0300

    Initialize JobBoard skeleton with arrays, mock data, and method skeletons

--- JobBoard.java
+++ JobBoard.java
@@ -2,6 +2,7 @@ public class JobBoard {
     private User[] users;
     private Company[] companies;
     private Job[] jobs;
+
     private int userCount;
     private int companyCount;
     private int jobCount;
@@ -10,14 +11,17 @@ public class JobBoard {
         this.users = new User[100];
         this.companies = new Company[50];
         this.jobs = new Job[200];
+
+
         this.userCount = 0;
         this.companyCount = 0;
         this.jobCount = 0;
+
         loadMockData();
     }
 
     private void loadMockData() {
-        User defaultRecruiter = new User("admin_hr", "password123", "hr@jobboard.com", "050-1234567", true);
+        User defaultRecruiter = new User("admin_hr", "Pass123!", "hr@jobboard.com", "0501234567", true);
         users[userCount++] = defaultRecruiter;
 
         Company google = new Company("Google", "High-Tech", 150000);
@@ -28,7 +32,7 @@ public class JobBoard {
         companies[companyCount++] = intel;
         companies[companyCount++] = amazon;
 
-        // 3. יצירת 10 משרות התחלתיות והכנסתן למערך (תוך קידום המונה)
+        // 3. יצירת 10 משרות התחלתיות והכנסתן למערך (שים לב לקידום המונה jobCount++)
         jobs[jobCount++] = new Job("Java Developer", 25000, true, google, defaultRecruiter);
         jobs[jobCount++] = new Job("Python Intern", 12000, false, google, defaultRecruiter);
         jobs[jobCount++] = new Job("Hardware Engineer", 28000, true, intel, defaultRecruiter);
@@ -41,31 +45,26 @@ public class JobBoard {
         jobs[jobCount++] = new Job("Product Manager", 32000, true, google, defaultRecruiter);
     }
 
-    // רישום משתמש חדש במערכת
-    public boolean registerUser(User user) {
-        // TODO: לממש לוגיקת הוספה למערך ובדיקת כפילויות
+    // מתודה לרישום משתמש
+    public boolean createUser(User newUser) {
+        // TODO: לממש לוגיקת רישום (בדיקת כפילויות, הוספה למערך וקידום מונה)
         return false;
     }
 
-    // התחברות למערכת (אימות שם משתמש וסיסמה)
+    // מתודה להתחברות
     public User login(String username, String password) {
-        // TODO: לממש לוגיקת סריקה במערך והתאמת סיסמה
+        // TODO: לממש לוגיקת התחברות (חיפוש במערך ואימות סיסמה)
         return null;
     }
 
-    // הוספת משרה חדשה למערכת
+    // מתודה להוספת משרה
     public boolean addNewJob(Job job) {
-        // TODO: לממש לוגיקת הוספת משרה וקידום המונה
+        // TODO: לממש לוגיקת הוספת משרה למערך וקידום המונה
         return false;
     }
 
-    // הדפסת כל המשרות הקיימות במערכת
+    // מתודה להדפסת כל המשרות
     public void printAllJobs() {
-        // TODO: לממש לולאה הרצה על מערך המשרות ומדפיסה אותן
+        // TODO: לממש לולאה שעוברת על מערך המשרות ומדפיסה אותן
     }
-
-    // --- Getters זמניים למקרה שנצטרך לבדוק את הגדלים של המערכים מבחוץ ---
-    public int getUserCount() { return userCount; }
-    public int getCompanyCount() { return companyCount; }
-    public int getJobCount() { return jobCount; }
 }
